28865d716d095b335872ea5fa85c3fed05a40a68
weijun <u11219043@o365.ttu.edu.tw>
2024-05-06 19:52:52 +0800

    測試用bash上傳

--- 0506hw2.c
+++ 0506hw2.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
+#include <string.h>
+
+void drawMenu()
+{ 
+    //因為這個函數的型式是void，所以在宣告的時候
+    //小括號裡面可以不用放任何東西
+    //可以在裡面設定參數(變數)
+    //設定一個函數用來畫
+    printf("--------------------------\n");
+    printf("|                        |\n");
+    printf("|                        |\n");
+    printf("|                        |\n");
+    printf("|  a. 畫出直角三角形     |\n");
+    printf("|  b. 顯示乘法表         |\n");
+    printf("|  c. 結束               |\n");
+    printf("|                        |\n");
+    printf("|                        |\n");
+    printf("--------------------------\n");
+}
 
 int main()
 {
+    //這是註解
+    
+    
+    
+    
+    
+    
     return 0;
-    //我想休學
-    //幹你娘
 }
