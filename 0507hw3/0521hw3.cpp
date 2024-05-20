d21f4ccc10e74b8ba8f730271f0f6b6374fb6949
weijun <u11219043@o365.ttu.edu.tw>
2024-05-21 01:13:47 +0800

    1.新增52-121行(整個選單的switch函數) 2.格式化增加可讀性

--- 0507hw3/0521hw3.cpp
+++ 0507hw3/0521hw3.cpp
@@ -48,3 +48,74 @@ int main()
             choice = tolower(choice); 
             //0516 Need to look up the definition of tolower() function 
             //tolower() function converts uppercase letters to lowercase directly, so no need to write four more lines for 'case 'A'' in case
+           
+            switch (choice)
+            {
+                case 'a':
+                    showSeats(seats);
+                    pressToContinue();
+                    system("cls"); // Clear screen and return to main menu
+                    break;
+
+                case 'b':
+                    printf("\nHow many seats do you need (1-4)? ");
+                    int numSeats;
+                    fflush(stdin);
+                    scanf("%d", &numSeats);
+                    if (numSeats < 1 || numSeats > 4) // If the number of seats is out of range
+                    {
+                        printf("\nInput error, please enter a number between 1-4.\n");
+                        pressToContinue();
+                    }
+                    else if (suggestSeats(seats, numSeats)) // If the number of seats is within range, execute the seat selection function
+                    {
+                        showSeats(seats);
+                        printf("\nAre you satisfied with this seat arrangement (y/n)?: ");
+                        char response; // Set a variable to store the user's input character for accepting the seats
+                        fflush(stdin); // Clear the screen
+                        scanf(" %c", &response);
+                        if (tolower(response) == 'y') // If the user accepts the seat selection result
+                        {
+                            // If the user accepts the seat selection result, change the selected seats' symbol from "-" to "@"
+                            acceptSuggestedSeats(seats); 
+                            system("cls"); // Clear screen and return to main menu
+                        }
+                        else if (tolower(response) == 'n')
+                        {
+                            notacceptSuggestedSeats(seats); // Reject the seats, cancel all chosen seats, prepare for reselection
+                            system("cls");               
+                        }
+                        else // If the user inputs an invalid character (not within options)
+						{
+							printf("\nInvalid input, please re-enter.\n");
+							notacceptSuggestedSeats(seats); // Revert temporarily marked seats
+							pressToContinue();
+							system("cls");
+						}
+                    }
+                    else // If not enough seats available, prompt user to press any key to return to the main menu
+                    {
+                        printf("\nNot enough seats available.\n");
+                        pressToContinue();
+                    }
+                    break;
+                    
+                case 'c': 
+                    // If 'c. Choose by yourself' is selected from the menu
+                    // Enter the seat map to let the user choose seats freely
+                    chooseSeats(seats);
+                    break;
+
+                case 'd':
+                    keepRunning = exitConfirmation();
+                    // Determine when to exit the loop (press 'N' to exit the loop)
+                    
+                    // After pressing 'd', the user can input 'y', 'Y', 'n', or 'N', handled by the exitConfirmation() function
+                    // After entering the function, if successful: return value is 1, if failed return value is 0.
+                    // If the returned value is 0,
+                    // keepRunning will be set to 0,
+                    // Then proceed to be blocked by while(keepRunning),
+                    // Because while(0) means false, it will be considered not executable,
+                    // And finally return 0;
+
+                    break;
