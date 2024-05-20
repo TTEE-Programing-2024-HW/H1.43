a30830daecdcd0da79c3f51e68cd12fcaa3fbf88
weijun <u11219043@o365.ttu.edu.tw>
2024-05-21 01:06:25 +0800

    1.使用VScode的格式化文件功能，整理註解的排版

--- 0507hw3/0521hw3.cpp
+++ 0507hw3/0521hw3.cpp
@@ -17,16 +17,16 @@
 
 // Predefine the function prototypes
 // The first letter of the function name should not be uppercase
-int checkPassword(); // Check password
-void chooseSeats(int seats[ROWS][COLS]); // Choose seats
-void showSeats(int seats[ROWS][COLS]); // Display booked seats
-void initializeSeats(int seats[ROWS][COLS]); // Initialize the seat map
-void pressToContinue(); // "Press any key to continue" for convenience
-int exitConfirmation(); // "Exit menu" functionality
-void menu(); // Display the main menu for the first option
-void notacceptSuggestedSeats(int seats[ROWS][COLS]); // Reject suggested seats, cancel all chosen seats, prepare for reselection
+int checkPassword();                                   // Check password
+void chooseSeats(int seats[ROWS][COLS]);               // Choose seats
+void showSeats(int seats[ROWS][COLS]);                 // Display booked seats
+void initializeSeats(int seats[ROWS][COLS]);           // Initialize the seat map
+void pressToContinue();                                // "Press any key to continue" for convenience
+int exitConfirmation();                                // "Exit menu" functionality
+void menu();                                           // Display the main menu for the first option
+void notacceptSuggestedSeats(int seats[ROWS][COLS]);   // Reject suggested seats, cancel all chosen seats, prepare for reselection
 int suggestSeats(int seats[ROWS][COLS], int numSeats); // Auto seat selection functionality
-void acceptSuggestedSeats(int seats[ROWS][COLS]); // Accept seats (after manual or auto selection, ask if user accepts)
-int findFourSeats(int seats[ROWS][COLS]); 
+void acceptSuggestedSeats(int seats[ROWS][COLS]);      // Accept seats (after manual or auto selection, ask if user accepts)
+int findFourSeats(int seats[ROWS][COLS]);
 // Open the seat selection function after user clicks auto-select
 // (because the logic for selecting four seats is different from general seat selection logic, mainly in the arrangement)
