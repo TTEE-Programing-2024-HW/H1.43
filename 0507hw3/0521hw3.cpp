a72c774163773fb9641b5461f5d892f3e8aac867
weijun <u11219043@o365.ttu.edu.tw>
2024-05-21 00:59:26 +0800

    1.本來土炮的寫，改成用很多函數。
    2.這次將檔案推到GITHUB的過程中，也遇到問題，且英文能力不足，所以程式、註解都確認無誤才上傳。但我還是會將遇到的問題稍微敘述。
    在這邊先說聲抱歉。

---
+++ 0507hw3/0521hw3.cpp
@@ -0,0 +1,23 @@
+// 0521hw3_H1.43ฎ}บ๛งก
+#include <stdio.h>
+#include <stdlib.h>
+// For using the rand() function
+#include <time.h>
+// For using the time() function
+#include <ctype.h> 
+// To use the tolower() function from the <ctype.h> library
+// (to convert uppercase to lowercase)
+#include <conio.h>
+// To use getch() and getche() for reading keyboard input
+
+#define ROWS 9 
+#define COLS 9
+// Define two constants and set both ROWS and COLS to 9,
+// ensuring their values remain constant throughout the code
+
+// Predefine the function prototypes
+// The first letter of the function name should not be uppercase
+
+
+
+
